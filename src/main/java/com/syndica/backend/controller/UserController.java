174823af8cd12a4594e90babdcf002b5d06d72bc
rodrigo dos santos <rodri.sant.dev@outlook.com>
2026-09-13 19:24:14 -0300

    configuracao de tema

--- src/main/java/com/syndica/backend/controller/UserController.java
+++ src/main/java/com/syndica/backend/controller/UserController.java
@@ -1,5 +1,6 @@
 package com.syndica.backend.controller;
 
+import java.util.Set;
 import java.util.UUID;
 import java.util.concurrent.TimeUnit;
 
@@ -8,17 +9,23 @@ import org.springframework.http.CacheControl;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.syndica.backend.domain.dto.ChangeUserThemeDTO;
 import com.syndica.backend.domain.dto.DocumentDownload;
 import com.syndica.backend.domain.models.User;
 import com.syndica.backend.domain.repositories.UserRepository;
 import com.syndica.backend.execptions.NotFoundException;
 import com.syndica.backend.execptions.UserDoesNotExistExecption;
 import com.syndica.backend.service.DocumentService;
+
+import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("user/")
@@ -35,7 +42,7 @@ public class UserController {
     }
 
     @GetMapping(value="/avatar-image/{id}")
-    public  ResponseEntity<InputStreamResource> getAvatarImage(
+    public ResponseEntity<InputStreamResource> getAvatarImage(
         @PathVariable UUID id
     ){
         User user = userRepository.findById(id)
@@ -59,4 +66,20 @@ public class UserController {
         .body(new InputStreamResource(document.content()));
     }
     
+    @PatchMapping("/update-theme")
+    public ResponseEntity<?> changeUserThemeController(
+        @AuthenticationPrincipal User user,
+        @RequestBody @Valid ChangeUserThemeDTO dto
+    ) {
+        Set<String> themePermited = Set.of("light", "dark", "system");
+
+        if (!themePermited.contains(dto.theme())) {
+            return ResponseEntity.badRequest().body("Tema inválido: " + dto.theme());
+        }
+
+        user.setThemePreference(dto.theme());
+        userRepository.save(user);
+        return ResponseEntity.noContent().build();
+    }
+
 }
